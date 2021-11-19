3cb1e45f716ac17124b063110e3870e8ead38bb1
Kentaro Matsutani <tiropetz@gmail.com>
2021-11-19 15:30:35 +0900

    chore: eslint rule追加

---
+++ packages/next/configs/eslint/js.js
@@ -0,0 +1,15 @@
+const { OFF, WARN, ERROR } = require("./const")
+
+module.exports = {
+  rules: {
+    complexity: [ERROR, 5],
+    "max-depth": [ERROR, 3],
+    "max-lines": [ERROR, { max: 50 }],
+    "max-nested-callbacks": [ERROR, 3],
+    "max-statements": [ERROR, 10],
+    "multiline-comment-style": OFF,
+    "no-magic-numbers": WARN,
+    "no-undefined": ERROR,
+    "sort-keys": WARN,
+  },
+}
