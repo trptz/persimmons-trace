fa8c5f22c31f407502f0b710a36bf52cfb81dbc4
Kentaro Matsutani <tiropetz@gmail.com>
2021-11-26 14:57:05 +0900

    chore: fix lint error

--- packages/next/src/pages/api/hello.ts
+++ packages/next/src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const STATUS_CODE ={
+  OK: 200
+}as const
+
 type Data = {
   name: string
 }
 
 export default function handler(
+  // eslint-disable-next-line unicorn/prevent-abbreviations
   req: NextApiRequest,
+  // eslint-disable-next-line unicorn/prevent-abbreviations
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(STATUS_CODE.OK).json({ name: 'John Doe' })
 }
