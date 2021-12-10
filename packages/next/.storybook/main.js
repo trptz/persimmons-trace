c1b95755b0df4302ff86c7cca2a872672086d364
Kentaro Matsutani <tiropetz@gmail.com>
2021-12-10 16:27:50 +0900

    chore: jest, storybookが動く

---
+++ packages/next/.storybook/main.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.@(ts|tsx)",
+  ],
+  features: { previewCsfV3: true },
+}
---
+++ packages/next/src/components/atoms/Checkbox/index.stories.tsx
@@ -0,0 +1,13 @@
+import { Checkbox } from ".";
+
+export default {
+  component: Checkbox,
+};
+
+export const Default = {};
+
+export const Checked = {
+  play: async () => {
+    // await userEvent.click(screen.getByText("Check"));
+  },
+};
